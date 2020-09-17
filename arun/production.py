54932ede290c205a5bf816d49a94fc2d9a69f418
Ashok <vashokkumar600@gmail.com>
2020-09-18 01:54:06 +0530

    aa

---
+++ arun/production.py
@@ -0,0 +1,19 @@
+import os
+from urllib.parse import urlparse
+
+
+DEBUG = False
+ALLOWED_HOSTS = ['notifier999.herokuapp.com']
+SECRET_KEY = os.environ.get('SECRET_KEY', 'arun')  
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
--- arun/settings.py
+++ arun/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'arun'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://notifier896.herokuapp.com','127.0.0.1']
 
 
 WSGI_APPLICATION = 'arun.wsgi.application'
